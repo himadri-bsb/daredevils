f339f8f0a95ae58b87804a8a32d00f278ab88601
Mohan <mohan@hike.in>
2015-04-25 22:59:01 +0530

    add card cell

---
+++ Buytc/Buytc/CardCell.h
@@ -0,0 +1,17 @@
+//
+//  CardCell.h
+//  Buytc
+//
+//  Created by Mohan on 25/04/15.
+//  Copyright (c) 2015 Himadri. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardCell : UITableViewCell
+
+@property (nonatomic, strong) UIImageView *cardImageView;
+@property (nonatomic, strong) UILabel *sizeLabel;
+@property (nonatomic, strong) UILabel *priceLabel;
+
+@end
