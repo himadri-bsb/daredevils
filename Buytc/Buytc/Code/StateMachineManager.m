b0e67644ffb8ba05f212d4c2ed12d8f08d63d372
Jatin <jatin@hike.in>
2015-04-26 06:48:53 +0530

    Add message after saying bye

--- Buytc/Buytc/Code/StateMachineManager.m
+++ Buytc/Buytc/Code/StateMachineManager.m
@@ -108,6 +108,7 @@
 
 - (void)userRepliedWithText:(NSString *)reply {
     if ([reply localizedCaseInsensitiveContainsString:@"Cancel"] || [reply localizedCaseInsensitiveContainsString:@"Done"] || [reply localizedCaseInsensitiveContainsString:@"Bye"]) {
+        [self.chatDelegate displayText:@"It is always nice talking to you, Jatin"];
         [self resetStateMachine];
     } else {
         [self manageStatesForUserReply:reply];
